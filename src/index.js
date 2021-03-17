d3bc97f1b58ef22cd5d39531e945d7b8c056f377
Wallace Rossini <rossiniwallace@gmail.com>
2021-03-17 13:48:39 -0300

    Add Cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 3000
+
+app.use(cores())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
   res.send("Hello World")
